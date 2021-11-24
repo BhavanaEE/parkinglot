ded3c14688d057ae8aab26a9fa4d572120bd01ec
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-11-24 13:25:06 +0530

    Updated park vehicle method in parkinglot class

--- src/main/java/com/everest/parkinglot/ParkingLot.java
+++ src/main/java/com/everest/parkinglot/ParkingLot.java
@@ -1,10 +1,15 @@
 package com.everest.parkinglot;
 
+import com.everest.parkinglotstrategy.*;
+import com.everest.parkinglotstrategy.ParkingStrategy;
+import com.everest.vehicle.Vehicle;
+
 public class ParkingLot {
     private String parkingId;
     private int noOfFloors;
     private int slotsInEachFloor;
     private Floor[] floors;
+    ParkingStrategy parkingStrategy;
 
     public ParkingLot(String parkingId, int noOfFloors, int slotsInEachFloor) {
         this.parkingId=parkingId;
@@ -26,5 +31,24 @@ public class ParkingLot {
 
     public int getNoOfFloors() {
         return this.noOfFloors;
+    }
+
+    public String parkVehicle(Vehicle vehicle, ParkingLot parkingLot) {
+        parkingStrategy=getParkingLotStrategyFor(vehicle.getVehicleType());
+        return parkingStrategy.parkVehicle(vehicle, parkingLot);
+    }
+
+    private ParkingStrategy getParkingLotStrategyFor(String vehicleType) {
+        switch (vehicleType){
+            case "CAR":
+                return new ParkByCarStrategy();
+
+            case "BIKE":
+                return new ParkByBikeStrategy();
+
+            case "TRUCK":
+                return new ParkByTruckStrategy();
+        }
+        return null;
     }
 }
