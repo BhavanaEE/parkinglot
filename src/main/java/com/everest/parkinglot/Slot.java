ab026b9d460c536adddcef2718e672bbeba4adfc
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-11-24 12:57:34 +0530

    Updated Slot class

--- src/main/java/com/everest/parkinglot/Slot.java
+++ src/main/java/com/everest/parkinglot/Slot.java
@@ -1,2 +1,24 @@
-package com.everest.parkinglot;public class Slot {
+package com.everest.parkinglot;
+
+import com.everest.vehicle.Vehicle;
+
+public class Slot {
+    Vehicle vehicle;
+    private String ticketId;
+    public void parkVehicle(Vehicle vehicle){
+        this.vehicle=vehicle;
+    }
+    public void unParkVehicle(){
+        this.vehicle=null;
+    }
+
+    public boolean isEmpty(){
+        return this.vehicle==null;
+    }
+    public void setTicketId(int i,int j){
+        this.ticketId="PR1234"+"_"+(i+1)+"_"+(j+1);
+    }
+    public String getTicketId(){
+        return this.ticketId;
+    }
 }
