a1d53db61569a446edcf3c37042304ceac0c2d64
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-11-24 14:43:23 +0530

    Updated unpark method in parkstrategy class

--- src/main/java/com/everest/parkinglotstrategy/ParkingStrategy.java
+++ src/main/java/com/everest/parkinglotstrategy/ParkingStrategy.java
@@ -6,7 +6,19 @@ import com.everest.vehicle.Vehicle;
 public abstract class ParkingStrategy {
     public abstract String parkVehicle(Vehicle vehicle, ParkingLot parkingLot);
 
-    public boolean unParkVehicle(String ticketId,ParkingLot parkingLot){
-        return false;
+    public String unParkVehicle(String ticketId,ParkingLot parkingLot){
+        try {
+            String[] getParticularFloorAndSlot = ticketId.split("_");
+            int floorNumber = Integer.parseInt(getParticularFloorAndSlot[1]) - 1;
+            int slotNumber = Integer.parseInt(getParticularFloorAndSlot[2]) - 1;
+            if (!parkingLot.getFloorAtIndex(floorNumber).getASlotAtIndex(slotNumber).isEmpty()) {
+                Vehicle vehicle1 = parkingLot.getFloorAtIndex(floorNumber).getASlotAtIndex(slotNumber).unParkVehicle();
+                return "UnParked vehicle with Registration Number: " + vehicle1.getRegistrationNumber() + " and Color: " + vehicle1.getColor();
+            }
+            return "Invalid ticket";
+        }
+        catch (Exception e){
+            return "Invalid Ticket";
+        }
     }
 }
